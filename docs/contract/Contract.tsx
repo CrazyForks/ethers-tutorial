a95e00a63699f33fd6a04998bc8b59c6b7853553
xjh22222228 <xjh22222228@gmail.com>
2025-05-28 22:40:02 +0800

    docs: update

--- docs/contract/Contract.tsx
+++ docs/contract/Contract.tsx
@@ -4,20 +4,25 @@
  *--------------------------------------------------------------------------------------------*/
 import React from "react";
 import { ethers } from "ethers";
-import { Button, notification } from "antd";
+import { Button, notification, Modal, Form, Input } from "antd";
 
-const provider = new ethers.JsonRpcProvider(
-  "https://rpc.buildbear.io/outstanding-juggernaut-05cd9cc5"
-);
+const RPC = "https://rpc.buildbear.io/outstanding-juggernaut-05cd9cc5";
 
-const Contract: React.FC = () => {
+const Component: React.FC = () => {
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
 
-  // 查询合约信息
-  async function getContract() {
-    notification.destroy();
+  // 查询余额
+  async function sendERC20Transaction() {
     setLoading(true);
+    notification.destroy();
     try {
+      const values = await form.getFieldsValue();
+
+      // 配置提供者（例如 buildbear、Infura、Alchemy 或本地节点）
+      const provider = new ethers.JsonRpcProvider(values.RPC);
+
       // 编写交互ABI，后面会介绍ABI是什么
       const abi = [
         "function balanceOf(address owner) view returns (uint256)",
@@ -26,7 +31,7 @@ const Contract: React.FC = () => {
       ];
       // 创建代币合约实例
       const contract = new ethers.Contract(
-        "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
+        values.contractAddress,
         abi,
         provider
       );
@@ -42,14 +47,18 @@ const Contract: React.FC = () => {
         message: "合约信息：",
         description: (
           <>
-            <div>余额(wei)：{balance}</div>
+            <div>余额(wei)：{String(balance)}</div>
+            <div>
+              余额({symbol})：{ethers.formatUnits(balance, decimals)}
+            </div>
             <div>代币符号：{symbol}</div>
-            <div>代币小数位数：{decimals}</div>
+            <div>代币小数位数：{String(decimals)}</div>
           </>
         ),
       });
     } catch (error: any) {
       notification.error({
+        duration: 0,
         message: "Error",
         description: error.message,
       });
@@ -57,11 +66,63 @@ const Contract: React.FC = () => {
     setLoading(false);
   }
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <Button type="primary" onClick={getContract} loading={loading}>
-      查询合约信息 Contract
-    </Button>
+    <>
+      <Modal
+        title="查询合约信息 Contract"
+        open={isModalOpen}
+        onOk={sendERC20Transaction}
+        onCancel={handleCancel}
+        confirmLoading={loading}
+      >
+        <Form
+          form={form}
+          preserve={false}
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 20 }}
+          autoComplete="off"
+        >
+          <Form.Item
+            initialValue={RPC}
+            label="RPC地址"
+            name="RPC"
+            rules={[{ required: true, message: "请输入提供商的测试RPC" }]}
+          >
+            <Input placeholder="请输入提供商的测试RPC" />
+          </Form.Item>
+
+          <Form.Item
+            initialValue="0x2cFC43B94126595E8B636fed9fB585fF220Bc97d"
+            label="钱包地址"
+            name="address"
+            rules={[{ required: true, message: "请输入ETH地址" }]}
+          >
+            <Input placeholder="请输入ETH地址" />
+          </Form.Item>
+
+          <Form.Item
+            initialValue={"0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"}
+            label="合约地址"
+            name="contractAddress"
+            rules={[{ required: true, message: "请输入合约代币地址" }]}
+          >
+            <Input placeholder="请输入合约代币地址" />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Button type="primary" onClick={showModal} loading={loading}>
+        查询合约信息 Contract
+      </Button>
+    </>
   );
 };
 
-export default Contract;
+export default Component;
